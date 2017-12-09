f9f3a992c521ca081f05c23363f719535cdcdcfe
Doug Woodward <woodw116@mail.chapman.edu>
2017-12-09 12:58:31 -0800

    actual log reg file

--- CS613/project/logistic_reg.r
+++ CS613/project/logistic_reg.r
@@ -1,40 +1,35 @@
-library(class)
+library(ISLR)
 set.seed(40)
 # load data
 data = read.csv("FlightPerformance.csv")
 
-delayed = data[,"delay"]
 
-clean = data[,c("carrier","dest","origin","schedtime", "weather", "dayweek", "distance","daymonth")]
+clean = data[,c("carrier","dest","origin","schedtime", "weather", "dayweek", "distance","daymonth","delay")]
 
 
 # replace categorical w/dummy variables
 clean$carrier = as.numeric(data$carrier)
 clean$dest = as.numeric(data$dest)
 clean$origin = as.numeric(data$origin)
+clean$delay = as.numeric(data$delay)
 
 # normalize all variables
-clean.data = scale(clean)
+maxs <- apply(clean, 2, max)
+mins <- apply(clean, 2, min)
+clean.data = scale(clean, center = mins, scale=maxs-mins)
 
 # test/train split
 trainIdx <- sample(seq(1, nrow(clean.data)), floor(nrow(clean.data) * 0.70))
-train.X <- clean.data[trainIdx,]
-test.X <- clean.data[-trainIdx,]
-train.Y <- delayed[trainIdx]
-test.Y <- delayed[-trainIdx]
+train <- as.data.frame(clean.data[trainIdx,])
+test <- as.data.frame(clean.data[-trainIdx,])
 
-# iterate over a number of values of K to get optimal
-# we could use gradient descent here if we wanted to get
-# tricky
-errors <- 0
-for (kval in 1:50){
-    knn.pred=knn(train.X, test.X, train.Y, k=kval)
-    errors[kval] <- mean(test.Y != knn.pred)
-}
-plot(errors, type="l")
-which.min(errors)
+glm.fits = glm(delay ~ ., family=binomial, data=train)
+summary(glm.fits)
 
-knn.pred=knn(train.X, test.X, train.Y, k=which.min(errors))
-table(knn.pred, test.Y)
-min(errors)
+#plot(errors, type="l")
+#which.min(errors)
+
+#knn.pred=knn(train.X, test.X, train.Y, k=which.min(errors))
+#table(knn.pred, test.Y)
+#min(errors)
 

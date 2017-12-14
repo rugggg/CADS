003ad881ff25b7d5bbd6ee8063e5ea643f7250a7
Doug Woodward <woodw116@mail.chapman.edu>
2017-12-13 20:56:46 -0800

    AIC and AIC+combos

--- CS555/final.r
+++ CS555/final.r
@@ -20,24 +20,24 @@ names(pension)
 # get summary stats
 summary(pension)
 
-# unset sink file
-sink()
-# histogram and boxplot each col
-for(col in 1:ncol(pension)){
-    print(names(pension)[col])
-    png(file=paste("hist_",names(pension)[col],".png"))
-    hist(pension[,col], main=names(pension)[col], xlab=names(pension)[col])
-    dev.off()
-    # skip box plotting wealth
-    if((names(pension)[col]) != "wealth89"){
-        png(file=paste("boxplot_",names(pension)[col],".png"))
-        f = as.formula(paste("wealth89 ~ ",names(pension)[col]))
-        boxplot(f, data=pension, main=names(pension)[col], xlab=names(pension)[col])
-        dev.off()
-    }
-}
 
-png(file="scatterplot_small.png")
+pension.clean  <-  na.omit(pension)
+# histogram and boxplot each col
+#for(col in 1:ncol(pension)){
+#    print(names(pension)[col])
+#    png(file=paste("hist_",names(pension)[col],".png"))
+#    hist(pension[,col], main=names(pension)[col], xlab=names(pension)[col])
+#    dev.off()
+#    # skip box plotting wealth
+#    if((names(pension)[col]) != "wealth89"){
+#        png(file=paste("boxplot_",names(pension)[col],".png"))
+#        f = as.formula(paste("wealth89 ~ ",names(pension)[col]))
+#        boxplot(f, data=pension, main=names(pension)[col], xlab=names(pension)[col])
+#        dev.off()
+#    }
+#}
+
+#png(file="scatterplot_small.png")
 # some options to create a higher res scatterplot
 #png(file="scatterplot",  height=10, width=10, units="in", res=1000, pointsize=4)
 #par(
@@ -47,5 +47,16 @@ png(file="scatterplot_small.png")
 #  cex.axis = 2,
 #  cex.lab  = 2
 #)
-scatterplotMatrix(pension)
-dev.off()
+#scatterplotMatrix(pension)
+#dev.off()
+
+base  <- lm(formula=wealth89 ~ .,data = pension.clean)
+step  <- stepAIC(base, trace = FALSE)
+step$anova
+
+step2  <- stepAIC(base, ~ .^2 + I(age)^2, trace = FALSE)
+step2$anova
+
+# unset sink file
+sink()
+

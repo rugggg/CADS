5e2efc8e10cae0e896ca03c6a0d181b1e683bf08
Doug Woodward <woodw116@mail.chapman.edu>
2018-02-12 20:35:03 -0800

    good spline fit - next steps - remove transits when fitting

--- thesis/test_dat/load_image.py
+++ thesis/test_dat/load_image.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 import matplotlib.pyplot as plt
 from astropy.io import fits
 from astropy.table import Table
@@ -11,6 +12,7 @@ from scipy.stats import zscore
 #hdulist = fits.open('kplr000892667-2009166043257_llc.fits')
 # use this one instead - it is a PC
 hdulist = fits.open('../data/kplr000757450-2009166043257_llc.fits')
+#hdulist = fits.open('../data/kplr000891901-2009131105131_llc.fits')
 # gather some basic info on the data
 #hdulist.info()
 lc_data = hdulist[1].data
@@ -22,10 +24,8 @@ col_data = hdulist[1].columns
 # zero in on the corrected flux data
 PDCSAP = lc_data['PDCSAP_FLUX']
 TIME = lc_data['TIME']
-print(TIME.shape)
 # print and display the light curve
 #print(PDCSAP)
-#plt.plot(PDCSAP)
 #plt.show()
 
 # now we need to flatten the light curve to remove low frequency 
@@ -34,13 +34,18 @@ print(TIME.shape)
 # the way the paper does this is to fit a basis spline to to the LC
 # and then divide by the best fit spline
 
-x = np.array([ 0. ,  1.2,  1.9,  3.2,  4. ,  6.5])
-y = np.array([ 0. ,  2.3,  3. ,  4.3,  2.9,  3.1])
+# we want to actually remove the transit events for flattening
 
 x = TIME[~np.isnan(PDCSAP)]
 y = PDCSAP[~np.isnan(PDCSAP)]
-
-calc_s = 0.05 * len(y) * np.nanvar(y)
+print(np.std(x))
+print(np.mean(x))
+#print([i for i in range(len(x)) if (abs(x[i] - np.mean(x)) > 1*np.std(x))])
+calc_s = 0.005 * len(y) * np.nanvar(y)
+print(calc_s)
+calc_s = len(y) + math.sqrt(2*len(y))
+print(calc_s)
+#(m-sqrt(2*m),m+sqrt(2*m))
 t,c,k = splrep(x, y, s=calc_s, k=5)
 #print('''\
 #        t:{}
@@ -48,21 +53,16 @@ t,c,k = splrep(x, y, s=calc_s, k=5)
 #        k:{}
 #'''.format(t,c,k))
 xmin, xmax = x.min(), x.max()
-xx = np.linspace(xmin, xmax, 100)
+xx = np.linspace(xmin, xmax, len(x))
 spline = BSpline(t,c,k,extrapolate=False)
 
-#plt.plot(x, y, 'bx', label='Original points')
-#plt.plot(xx,spline(xx), 'r', label='BSpline')
 
+plt.subplot(211)
+plt.plot(x, y, 'bx', label='Original points')
+plt.plot(xx,spline(xx), 'r', label='BSpline')
+plt.subplot(212)
 #try a divide
-#print(PDCSAP.shape)
-#print(arraymagU2.shape)
-#print(PDCSAP)
-#print(arraymagU2)
-#print(arraymagU)
-print(spline(x))
 flattened = np.divide(y, spline(x))
-print(flattened)
 plt.plot(x, flattened, 'g', label='Original points')
 
 plt.grid()

e9e972237cd91c75caafe8642987b3d5dc2fb434
Doug Woodward <woodw116@mail.chapman.edu>
2018-02-18 21:39:17 -0800

    folded light curves

--- thesis/test_dat/load_image.py
+++ thesis/test_dat/load_image.py
@@ -5,7 +5,7 @@ from astropy.io import fits
 from astropy.table import Table
 from scipy.interpolate import BSpline
 from scipy.interpolate import splrep
-from scipy.stats import zscore
+from scipy import stats
 
 # open using the astropy module
 # this one is an NTP 
@@ -38,12 +38,42 @@ TIME = lc_data['TIME']
 
 x = TIME[~np.isnan(PDCSAP)]
 y = PDCSAP[~np.isnan(PDCSAP)]
-print(np.std(x))
-print(np.mean(x))
-#print([i for i in range(len(x)) if (abs(x[i] - np.mean(x)) > 1*np.std(x))])
+#print(np.std(x))
+#print(np.mean(x))
+
+# detect all outliers and linearly interpolate
+#print([i for i in range(len(y)) if (abs(y[i] - np.mean(y)) > 2*np.std(y))])
+print("DATA 1006: ",y[1006])
+print("MEAN: ",np.mean(y))
+print("VAR: ",np.var(y))
+def find_bounds(index, outliers, left=1, right=1):
+    if left+index not in outliers and right+index not in outliers:
+        return left, right
+    if left+index in outliers:
+        left += 1
+    if right+index in outliers:
+        right += 1
+    return find_bounds(index, outliers, left, right)
+
+# oh - this works - but the problem is we need to use a sort of running/windowed average and std dev bc
+# what we get is actually not catching all the transit points as outliers bc the lc has high variabilit before
+# smoothing
+
+def interpolate_outliers(data, sigma=2):
+    new_data = data
+    idx = [i for i in range(len(data)) if (abs(data[i] - np.mean(data)) > sigma*np.std(data))]
+    # need to be recursive here - if index i is an outlier, but so is the previous one - then extend one that way by one and repeat as needed
+    for i in idx:
+        left_offset, right_offset = find_bounds(i, idx)
+        print("replacing index: ",i, "offset: -",left_offset," ",right_offset)
+        data[i] = data[i-left_offset] + (data[i+right_offset]-data[i-left_offset])
+    return new_data
+
+#y = interpolate_outliers(y)
+
+print("DATA 1006: ",y[1006])
 calc_s = 0.005 * len(y) * np.nanvar(y)
-print(calc_s)
-calc_s = len(y) + math.sqrt(2*len(y))
+#calc_s = len(y) + math.sqrt(2*len(y))
 print(calc_s)
 #(m-sqrt(2*m),m+sqrt(2*m))
 t,c,k = splrep(x, y, s=calc_s, k=5)
@@ -55,16 +85,46 @@ t,c,k = splrep(x, y, s=calc_s, k=5)
 xmin, xmax = x.min(), x.max()
 xx = np.linspace(xmin, xmax, len(x))
 spline = BSpline(t,c,k,extrapolate=False)
-
+# calc mean and std dev
+y_mean = [np.nanmean(PDCSAP)]*len(x)
+y_stddev = np.nanstd(PDCSAP)
+print(y_stddev)
 
 plt.subplot(211)
+# plot the mean and variance lines
+plt.plot(x,y_mean+y_stddev, 'b-',label='1 STD Dev')
+plt.plot(x,y_mean+2*y_stddev, 'g-',label='2 STD Dev')
+plt.plot(x,y_mean+3*y_stddev, 'y-',label='3 STD Dev')
+plt.plot(x,y_mean-y_stddev, 'b-')
+plt.plot(x,y_mean-2*y_stddev, 'g-')
+plt.plot(x,y_mean-3*y_stddev, 'y-')
+plt.plot(xx,y_mean,'r-',label="Mean line")
+# plot the original points
 plt.plot(x, y, 'bx', label='Original points')
+# plot the spline
 plt.plot(xx,spline(xx), 'r', label='BSpline')
+plt.legend(loc='best')
 plt.subplot(212)
 #try a divide
 flattened = np.divide(y, spline(x))
+# plot the resulting spline
 plt.plot(x, flattened, 'g', label='Original points')
-
 plt.grid()
+plt.legend(loc='best')
+
+# try fitting and dividing again
+calc_s_flat = len(flattened) + math.sqrt(2*len(flattened))
+print(calc_s_flat)
+t2,c2,k2 = splrep(x, flattened, s=calc_s_flat, k=3)
+print('''\
+        t:{}
+        c:{}
+        k:{}
+'''.format(t2,c2,k2))
+xx = np.linspace(xmin, xmax, len(x))
+spline2 = BSpline(t2,c2,k2,extrapolate=False)
+print(spline2)
+plt.plot(xx,spline2(xx), 'r', label='BSpline2')
+
 plt.legend(loc='best')
 plt.show()

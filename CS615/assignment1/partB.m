0b1a48ee7a6507d227b0f1c73fda0a74e9e8fd9e
Doug Woodward <woodw116@mail.chapman.edu>
2018-02-26 19:15:34 -0800

    some thesis stuff and other

--- CS615/assignment1/partB.m
+++ CS615/assignment1/partB.m
@@ -7,3 +7,22 @@ find(A < 3);
 result1 = [A > 155];
 colormap(gray),imagesc(result1)
 
+# from his code
+I=imread('mammogram.jpg');
+I=double(I);
+Tnew=.5(max(I(:))+min(I(:)));
+Told=0;
+T0=10^(-8)
+while(abs(Tnew-Told)>T))
+  Q1=I((I>Tnew));
+  Q2=I((I<Tnew));
+  x1=mean(Q1);
+  x2=mean(Q2);
+  
+  Told=Tnew;
+  Tnew=mean([x1,x2]);
+end
+
+result=(I>Tnew)
+colormap(gray)
+subplot(1,2,1)
